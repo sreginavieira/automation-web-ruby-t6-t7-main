4323e1160d50bf8bfe934ee0a6a459076a4263ef
Sara Vieira <85461821+sreginavieira@users.noreply.github.com>
2022-08-10 00:16:11 -0300

    Ajustes finais para entrega do projeto

--- automation-web-ruby-t6-t7-main/features/step_definitions/home.rb
+++ automation-web-ruby-t6-t7-main/features/step_definitions/home.rb
@@ -4,32 +4,32 @@ end
 
 Então('devo visualizar a informação {string}') do |titleHomePage|
     home.validate_text_homePage(titleHomePage)
-    #sleep 3
+    sleep 2
 end
 
 Quando('clico em {string}') do |sobrenos|
     home.click_link(sobrenos)
-    #sleep 3
+    sleep 2
 end
   
   Então('visualizo o menu {string}') do |title_sobrenos|
     home.validade_text_sobrenos(title_sobrenos)
-    #sleep 3
+    sleep 3
    end
 
    Quando('vou ao menu {string}') do |depoimentos|
     home.click_link(depoimentos)
-    #sleep 3
+    sleep 2
   end
   
   Então('vejo o texto {string}') do |texto_depoimentos|
   home.validade_text_depoimentos(texto_depoimentos)
-    #sleep 3
+    sleep 3
   end
 
   Quando('acesso o menu {string}') do |parceiros|
   home.click_link(parceiros)
-   sleep 3
+   sleep 2
   end
   
   Então('consulto o menu {string}') do |title_parceiros|
@@ -39,7 +39,7 @@ end
 
   Quando('carregar o menu {string}') do |faleconosco|
   home.click_link(faleconosco)
-    sleep 3
+    sleep 2
   end
   
   Então('devo ver o menu {string}') do |title_faleconosco|
